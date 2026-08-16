baf0a49ca437e81ffebdcac1b06f9a85713ad15e
adi_random <me.adi.hf@gmail.com>
2026-08-16 11:18:04 +0300

    Fix fibo.s for intel asm

--- tests/from_c/fibo.s
+++ tests/from_c/fibo.s
@@ -2,21 +2,15 @@
 
 .section .rodata
 .LC0:
-        .string "%d\n"
+        .string "Enter a non-negative integer (n): "
 .LC1:
-        .string "F(%d) = %d + %d = %d\n"
-.LC2:
-        .string "r"
-.LC3:
-        .string "fibo.txt"
-.LC4:
-        .string "Error: could not open fibo.txt"
-.LC5:
-        .string "Skipping negative value %d\n"
-.LC6:
-        .string "Fibonacci term F(%d) = %lld\n"
-.LC7:
         .string "%d"
+.LC2:
+        .string "Error: Please enter a valid non-negative integer."
+.LC3:
+        .string "%d\n"
+.LC4:
+        .string "Fibonacci term F(%d) = %lld\n"
 
 .text
 .globl fibonacci
@@ -25,11 +19,6 @@ fibonacci:
         mov     rbp, rsp
         sub     rsp, 32
         mov     DWORD PTR [rbp-20], edi
-        mov     eax, DWORD PTR [rbp-20]
-        mov     esi, eax
-        lea     rdi, [rip + .LC0]
-        mov     eax, 0
-        call    printf
         cmp     DWORD PTR [rbp-20], 0
         jg      .L2
         mov     eax, 0
@@ -54,15 +43,6 @@ fibonacci:
         mov     eax, DWORD PTR [rbp-8]
         add     eax, edx
         mov     DWORD PTR [rbp-12], eax
-        mov     esi, DWORD PTR [rbp-12]
-        mov     ecx, DWORD PTR [rbp-8]
-        mov     edx, DWORD PTR [rbp-4]
-        mov     eax, DWORD PTR [rbp-20]
-        mov     r8d, esi
-        mov     esi, eax
-        lea     rdi, [rip + .LC1]
-        mov     eax, 0
-        call    printf
         mov     eax, DWORD PTR [rbp-12]
         cdqe
 .L3:
@@ -73,51 +53,42 @@ fibonacci:
 main:
         push    rbp
         mov     rbp, rsp
-        sub     rsp, 32
-        lea     rsi, [rip + .LC2]
-        lea     rdi, [rip + .LC3]
-        call    fopen
-        mov     QWORD PTR [rbp-8], rax
-        cmp     QWORD PTR [rbp-8], 0
-        jne     .L8
-        lea     rdi, [rip + .LC4]
+        sub     rsp, 16
+        lea     rdi, [rip + .LC0]
+        mov     eax, 0
+        call    printf
+        lea     rax, [rbp-12]
+        mov     rsi, rax
+        lea     rdi, [rip + .LC1]
+        mov     eax, 0
+        call    __isoc23_scanf
+        cmp     eax, 1
+        jne     .L6
+        mov     eax, DWORD PTR [rbp-12]
+        test    eax, eax
+        jns     .L7
+.L6:
+        lea     rdi, [rip + .LC2]
         call    puts
         mov     eax, 1
-        jmp     .L11
-.L10:
-        mov     eax, DWORD PTR [rbp-20]
-        test    eax, eax
-        jns     .L9
-        mov     eax, DWORD PTR [rbp-20]
+        jmp     .L9
+.L7:
+        mov     eax, DWORD PTR [rbp-12]
         mov     esi, eax
-        lea     rdi, [rip + .LC5]
+        lea     rdi, [rip + .LC3]
         mov     eax, 0
         call    printf
-        jmp     .L8
-.L9:
-        mov     eax, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rbp-12]
         mov     edi, eax
         call    fibonacci
-        mov     QWORD PTR [rbp-16], rax
-        mov     eax, DWORD PTR [rbp-20]
-        mov     rdx, QWORD PTR [rbp-16]
+        mov     QWORD PTR [rbp-8], rax
+        mov     eax, DWORD PTR [rbp-12]
+        mov     rdx, QWORD PTR [rbp-8]
         mov     esi, eax
-        lea     rdi, [rip + .LC6]
+        lea     rdi, [rip + .LC4]
         mov     eax, 0
         call    printf
-.L8:
-        lea     rdx, [rbp-20]
-        mov     rax, QWORD PTR [rbp-8]
-        lea     rsi, [rip + .LC7]
-        mov     rdi, rax
         mov     eax, 0
-        call    __isoc23_fscanf
-        cmp     eax, 1
-        je      .L10
-        mov     rax, QWORD PTR [rbp-8]
-        mov     rdi, rax
-        call    fclose
-        mov     eax, 0
-.L11:
+.L9:
         leave
         ret
